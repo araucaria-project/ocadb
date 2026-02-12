f5c04f93ef4bda6d392842b66cd5c92e31507699
Bartlomiej Matuszkiewicz <bmatuszkiewicz@akond.com>
2026-02-12 13:26:28 +0100

    trailing slashes

--- api/routers/observations.py
+++ api/routers/observations.py
@@ -58,7 +58,7 @@ async def update_observation(
 
     return observation_data
 
-@router.get("/{id}/", response_description="Get a single Observation", response_model=Observation)
+@router.get("/{id}", response_description="Get a single Observation", response_model=Observation)
 async def get_observation(
         token: Annotated[str, Depends(AuthService.validate_token)],
         id: PydanticObjectId):
@@ -69,7 +69,7 @@ async def get_observation(
         raise HTTPException(status_code=404, detail=f"Observation with ID {id} not found")
     return observation
 
-@router.get("/{id}/url/", response_description="Get a presigned URL for a single Observation", response_model=S3PresignedUrl)
+@router.get("/{id}/url", response_description="Get a presigned URL for a single Observation", response_model=S3PresignedUrl)
 async def get_observation_url(
         token: Annotated[str, Depends(AuthService.validate_token)],
         id: PydanticObjectId,
@@ -98,7 +98,7 @@ async def list_observations(
     return observations
 
 
-@router.get("/by-filename/{filename}/", response_description="Get Observation by filename", response_model=List[Observation])
+@router.get("/by-filename/{filename}", response_description="Get Observation by filename", response_model=List[Observation])
 async def get_observation_by_filename(
         filename: str,
         token: Annotated[str, Depends(AuthService.validate_token)]):
@@ -112,7 +112,7 @@ async def get_observation_by_filename(
         raise HTTPException(status_code=404, detail=f"Observation with filename {filename} not found")
     return observations
 
-@router.post("/by-batch-filename/url/", response_description="Get Presigned URL by batch of filenames", response_model=List[S3PresignedUrl])
+@router.post("/by-batch-filename/url", response_description="Get Presigned URL by batch of filenames", response_model=List[S3PresignedUrl])
 async def get_batch_filename_url(
         filename_list: Annotated[List[str], Body(...)],
         token: Annotated[str, Depends(AuthService.validate_token)],
@@ -143,7 +143,7 @@ async def get_batch_filename_url(
 
     return url_responses
 
-@router.get("/by-filename/{filename}/url/", response_description="Get Presigned URL by filename", response_model=List[S3PresignedUrl])
+@router.get("/by-filename/{filename}/url", response_description="Get Presigned URL by filename", response_model=List[S3PresignedUrl])
 async def get_observation_by_filename_url(
         filename: str,
         token: Annotated[str, Depends(AuthService.validate_token)],
@@ -174,7 +174,7 @@ async def get_observation_by_filename_url(
     return url_responses
 
 
-@router.get("/by-object/{object_name}/", response_description="List Observations by object name", response_model=List[Observation])
+@router.get("/by-object/{object_name}", response_description="List Observations by object name", response_model=List[Observation])
 async def list_observations_by_object(
         object_name: str,
         token: Annotated[str, Depends(AuthService.validate_token)]):
@@ -187,7 +187,7 @@ async def list_observations_by_object(
     return observations
 
 
-@router.get("/by-filter/{filter_name}/", response_description="List Observations by filter", response_model=List[Observation])
+@router.get("/by-filter/{filter_name}", response_description="List Observations by filter", response_model=List[Observation])
 async def list_observations_by_filter(
         filter_name: str,
         token: Annotated[str, Depends(AuthService.validate_token)]):
@@ -255,7 +255,7 @@ async def search_multi(
 
     return observations
 
-@router.put("/{id}/metadata/", response_description="Update observation metadata")
+@router.put("/{id}/metadata", response_description="Update observation metadata")
 async def update_observation_metadata(
     id: PydanticObjectId,
     metadata: Annotated[Dict[str, Any], Body(...)],
@@ -273,7 +273,7 @@ async def update_observation_metadata(
     return {"message": "Metadata updated successfully", "observation_id": str(id)}
 
 
-@router.delete("/{id}/", response_description="Delete an Observation")
+@router.delete("/{id}", response_description="Delete an Observation")
 async def delete_observation(
     id: PydanticObjectId,
     token: Annotated[str, Depends(AuthService.validate_token)]
